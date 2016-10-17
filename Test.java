af2a92e4c9028f0ae63f541eee599ebf0f2fd3af
vikash7c <vikash7c@yahoo.com>
2016-10-17 12:15:25 +0530

    Add files via upload

---
+++ Test.java
@@ -0,0 +1,13 @@
+package com.test.admc;
+
+public class Test {
+public static void main(String[] args) {
+	if(3!=3){
+		System.out.println("first");
+	}else if(true){
+		System.out.println("middle");
+	}else if(true){
+		System.out.println("last");
+	}
+}
+}
